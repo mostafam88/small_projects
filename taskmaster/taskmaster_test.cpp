e66c5d37cddb06a3e90e7a5b4f552f6dfec8791e
Mostafa <mostafa.mrtazavi@gmail.com>
2022-03-11 00:31:26 +0100

    test fairness of the game

--- taskmaster/taskmaster_test.cpp
+++ taskmaster/taskmaster_test.cpp
@@ -23,4 +23,52 @@ TEST(TaskMasterTest, weightComparisonIsEitherHeavierOrLighter)
                 marble.m_weightComparison == WeightComparisonResult::lighter);
 }
 
+TEST(TaskMasterTest, isItReallyUniformRandom)
+{
+    std::array<int, Taskmaster::s_maxNumberOfMarbles> ids{};
+    std::array<int, 2>                                weightDiff{};
+
+    const int numberOfEvaluations{10000};
+
+    for (int i = 0; i < numberOfEvaluations; ++i)
+    {
+        SillyProjects::Taskmaster taskmaster{};
+        const auto                marble = taskmaster.getMarble();
+
+        ASSERT_GE(marble.m_id, 1);
+        ASSERT_LE(marble.m_id, Taskmaster::s_maxNumberOfMarbles);
+        ++ids[marble.m_id - 1];
+
+        if (marble.m_weightComparison ==
+            SillyProjects::WeightComparisonResult::heavier)
+        {
+            ++weightDiff[0];
+        } else
+        {
+            ASSERT_TRUE(marble.m_weightComparison ==
+                        WeightComparisonResult::lighter);
+            ++weightDiff[1];
+        }
+    }
+
+    const auto expectedProbability =
+        1.0F / float(Taskmaster::s_maxNumberOfMarbles);
+
+    const auto tolerance = 0.1F * expectedProbability;
+    const auto probabilityIsCloseToExpected =
+        [&expectedProbability, &numberOfEvaluations,
+         &tolerance](const int id) -> void {
+        const auto probability = id / float(numberOfEvaluations);
+        EXPECT_NEAR(probability, expectedProbability, tolerance);
+    };
+    std::for_each(ids.begin(), ids.end(), probabilityIsCloseToExpected);
+
+    const auto  binaryExpectedProbability{0.5F};
+    const float binaryTolerance{0.05F};
+    EXPECT_NEAR(weightDiff[0] / float(numberOfEvaluations),
+                binaryExpectedProbability, binaryTolerance);
+    EXPECT_NEAR(weightDiff[1] / float(numberOfEvaluations),
+                binaryExpectedProbability, binaryTolerance);
+}
+
 } // namespace SillyProjects
