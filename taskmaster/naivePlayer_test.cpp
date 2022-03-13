141ec959c478111ad1772717531b014aad78df99
Mostafa <mostafa.mrtazavi@gmail.com>
2022-03-14 00:09:34 +0100

    add test for naive player

---
+++ taskmaster/naivePlayer_test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include "naivePlayer.hpp"
+#include "taskmaster.hpp"
+
+
+namespace SillyProjects
+{
+
+TEST(NaivePlayerTest, alwaysSuccess)
+{
+    std::vector<int> guessedValues{};
+    int numRuns{0};
+    while (guessedValues.size() < Taskmaster::s_maxNumberOfMarbles)
+    {
+        ++numRuns;
+        SillyProjects::NaivePlayer player{};
+
+        auto       taskmaster = SillyProjects::Taskmaster::create();
+        const bool success    = taskmaster.play(player);
+        EXPECT_TRUE(success);
+
+        const int currentGuess = player.guessUniqueMarbleId();
+
+        const auto foundIt = std::find(guessedValues.begin(), guessedValues.end(), currentGuess);
+        if (foundIt == guessedValues.end()) {
+            guessedValues.push_back(currentGuess);
+        }
+    }
+
+    std::cout << '\n' << "             Guessed values: \n             ";
+    std::sort(guessedValues.begin(), guessedValues.end());
+    std::copy(begin(guessedValues), end(guessedValues), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << '\n' << "             Number of runs: " << numRuns << "\n\n";
+}
+
+} // namespace SillyProjects
