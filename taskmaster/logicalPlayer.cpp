6378e34eca40a118cc29c9a824d1b44bbf19dba7
Mostafa <mostafa.mrtazavi@gmail.com>
2022-04-10 15:57:48 +0200

    return the first set of random ids pair

--- taskmaster/logicalPlayer.cpp
+++ taskmaster/logicalPlayer.cpp
@@ -1,4 +1,8 @@
+#include <algorithm>
+#include <random>
+
 #include "logicalPlayer.hpp"
+#include "taskmaster.hpp"
 
 
 namespace SillyProjects
@@ -6,7 +10,40 @@ namespace SillyProjects
 
 Types::MarbleIdsPair LogicalPlayer::getFirstMarbleIdsPairToCompare() const
 {
-    return Types::MarbleIdsPair{};
+    // https://stackoverflow.com/questions/5008804/generating-random-integer-from-a-range
+    std::random_device                 rd;
+    std::mt19937                       rng(rd());
+    std::uniform_int_distribution<int> uni(1, Taskmaster::s_maxNumberOfMarbles);
+
+    Types::MarbleIds first{};
+    Types::MarbleIds second{};
+    while ((first.size() < 4))
+    {
+        const int  id = uni(rng);
+        const bool isNew =
+            (first.end() == std::find(first.begin(), first.end(), id));
+        if (isNew)
+        {
+            first.push_back(id);
+        }
+    }
+
+    while (second.size() < 4)
+    {
+        const int  id = uni(rng);
+        const bool isNew =
+            (first.end() == std::find(first.begin(), first.end(), id)) &&
+            (second.end() == std::find(second.begin(), second.end(), id));
+        if (isNew)
+        {
+            second.push_back(id);
+        }
+    }
+
+    std::sort(first.begin(), first.end());
+    std::sort(first.begin(), first.end());
+
+    return Types::MarbleIdsPair{first, second};
 }
 
 
